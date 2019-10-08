7585591feb931d2509daad88faeeb79ea3d95428
ludel <ludovic.delsol@ynov.com>
2019-10-08 18:58:08 +0200

    Update resource url

--- app/config.js
+++ app/config.js
@@ -1,6 +1,6 @@
 const prod = {
-    ASSETS_URL: 'http://localhost:8081/assets/',
-    API_URL: 'http://localhost:8080/',
+    ASSETS_URL: 'https://onregardequoi.com/assets/',
+    API_URL: 'http://51.75.27.95:8002',
 };
 
 const dev = {
