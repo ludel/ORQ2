ba7d298356ed16db5d2fa13cc5e1a74839747640
ludel <ludovic.delsol@ynov.com>
2019-10-14 14:25:48 +0200

    Add map routes

--- app.py
+++ app.py
@@ -17,18 +17,32 @@ def about():
 def app():
     return template('template/app/index.html')
 
+
+# App static files
 @get('/app.js')
 def app():
     return static_file('app.js', root='dist')
+
+@get('/app.js.map')
+def app():
+    return static_file('app.js.map', root='dist')
 
 @get('/app.css')
 def app():
     return static_file('app.css', root='dist')
 
+@get('/app.css.map')
+def app():
+    return static_file('app.css.map', root='dist')
+
+
+# Favicon
 @get('/favicon.ico')
 def app():
     return static_file('img/icon/favicon.ico', root='assets')
 
+
+# SEO static files
 @get('/robots.txt')
 def app():
     return static_file('seo/robots.txt', root='assets')
@@ -37,6 +51,8 @@ def app():
 def app():
     return static_file('seo/sitemap.xml', root='assets')
 
+
+# Static assets
 @get('/assets/<path:path>')
 def static(path):
     return static_file(path, root='assets')
